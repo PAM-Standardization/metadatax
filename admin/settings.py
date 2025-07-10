ef0dc2ec6136f1361f75585270896cc6b14a0269
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2025-07-10 08:40:54 +0200

    v0.4.4

--- admin/settings.py
+++ admin/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG").lower() == "True".lower()
+env_debug: str = os.environ.get("DEBUG")
+DEBUG = env_debug.lower() == "True".lower()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
