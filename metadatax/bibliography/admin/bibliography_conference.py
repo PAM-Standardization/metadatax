af39be88462f40e8b2e38ae866da06bb8952238c
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2025-07-10 10:09:37 +0200

    Add missing biblio admin

--- metadatax/bibliography/admin/bibliography_conference.py
+++ metadatax/bibliography/admin/bibliography_conference.py
@@ -1,16 +1,6 @@
 from django.contrib import admin
-from django.db import models
 
-
-class BibliographyConference(models.Model):
-    conference_name = models.CharField(
-        max_length=255, help_text="Required for a conference"
-    )
-    conference_location = models.CharField(
-        max_length=255,
-        help_text="Required for a conference (format: {City}, {Country})",
-    )
-    conference_abstract_book_url = models.URLField(null=True, blank=True)
+from metadatax.bibliography.models import BibliographyConference
 
 
 @admin.register(BibliographyConference)
