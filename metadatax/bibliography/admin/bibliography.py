a178513961a496fd99063dacf800fc59d79e4d6e
Elodie Morin <elodie.morin@ensta-bretagne.fr>
2025-07-16 10:29:17 +0200

    fix(bibliography): update form to correctly display errors related to constraints

--- metadatax/bibliography/admin/bibliography.py
+++ metadatax/bibliography/admin/bibliography.py
@@ -1,8 +1,99 @@
+from typing import Optional
+
+from django import forms
 from django.contrib import admin
+from django.core.exceptions import ValidationError
 
 from metadatax.bibliography.models import Bibliography
 from .author import AuthorInline
 from ...utils.admin import get_many_to_many
+
+
+class BibliographyForm(forms.ModelForm):
+    class Meta:
+        model = Bibliography
+        fields = "__all__"
+
+    def _check_field_empty(
+        self, biblio_type: Bibliography.Type, field_name: str, should_be_empty: bool
+    ):
+        data = self.cleaned_data.get(field_name)
+        if should_be_empty and data is not None:
+            raise ValidationError(
+                {
+                    field_name: f"This field should stay empty for a '{biblio_type.label}' type bibliography."
+                }
+            )
+
+    def clean(self):
+        super().clean()
+
+        status: Optional[Bibliography.Status] = self.cleaned_data.get("status")
+        if status is not None:
+            if (
+                status == Bibliography.Status.PUBLISHED
+                and self.cleaned_data.get("publication_date") is None
+            ):
+                raise ValidationError(
+                    {
+                        "publication_date": "This field is required for a 'Published' bibliography."
+                    }
+                )
+
+        b_type: Optional[Bibliography.Type] = self.cleaned_data.get("type")
+        if b_type is not None:
+            t = Bibliography.Type
+
+            self._check_field_empty(
+                t.SOFTWARE, "software_information", should_be_empty=False
+            )
+            self._check_field_empty(
+                t.SOFTWARE, "article_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.SOFTWARE, "conference_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.SOFTWARE, "poster_information", should_be_empty=True
+            )
+
+            self._check_field_empty(
+                t.ARTICLE, "software_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.ARTICLE, "article_information", should_be_empty=False
+            )
+            self._check_field_empty(
+                t.ARTICLE, "conference_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.ARTICLE, "poster_information", should_be_empty=True
+            )
+
+            self._check_field_empty(
+                t.CONFERENCE, "software_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.CONFERENCE, "article_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.CONFERENCE, "conference_information", should_be_empty=False
+            )
+            self._check_field_empty(
+                t.CONFERENCE, "poster_information", should_be_empty=True
+            )
+
+            self._check_field_empty(
+                t.POSTER, "software_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.POSTER, "article_information", should_be_empty=True
+            )
+            self._check_field_empty(
+                t.POSTER, "poster_information", should_be_empty=False
+            )
+
+        return self.cleaned_data
 
 
 @admin.register(Bibliography)
@@ -18,6 +109,7 @@ class BibliographyAdmin(admin.ModelAdmin):
     inlines = [
         AuthorInline,
     ]
+    form = BibliographyForm
 
     @admin.display(description="Tags")
     def show_tags(self, obj: Bibliography):
